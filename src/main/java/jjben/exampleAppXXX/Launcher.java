24e064a755924400e85fe73904b4052117d8fbc6
JjBenoit <jj_ben_2004@hotmail.com>
2023-02-24 03:17:50 +0100

    amelioration des packages

--- src/main/java/jjben/exampleAppXXX/Launcher.java
+++ src/main/java/jjben/exampleAppXXX/Launcher.java
@@ -1,40 +1,39 @@
-package jjben;
+package jjben.exampleAppXXX;
 
-
-import jjben.actors.ProducerThreadLog;
-import jjben.dto.OperationDto;
-import jjben.dto.OperationStatisticsDto;
-import jjben.dto.OperationStatisticsDtoFactory;
-import jjben.share.AsynchronousStatLogger;
-import jjben.writers.SimpleAggregatirWriter;
+import jjben.asynchstatlogger.fwk.share.AsynchronousStatLogger;
+import jjben.exampleAppXXX.actor.ProducerThreadLog;
+import jjben.exampleAppXXX.dto.OperationDto;
+import jjben.exampleAppXXX.dto.OperationStatisticsDto;
+import jjben.exampleAppXXX.dto.OperationStatisticsDtoFactory;
+import jjben.exampleAppXXX.writer.SimpleAggregatirWriter;
 
 public class Launcher {
-	
-	
-	
+
+
+
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		Launcher launcher = new Launcher();
 		launcher.start(10,1,10);
 	}
-	
+
 
 	public void start(int nbThreadProducer, int nbThreadConsummer,int aggragationPeriodictyInSeconds) throws InterruptedException {
-		
+
 		AsynchronousStatLogger<OperationDto, OperationStatisticsDto> queue = new AsynchronousStatLogger<OperationDto, OperationStatisticsDto>
 		(nbThreadConsummer,aggragationPeriodictyInSeconds, new SimpleAggregatirWriter(),new OperationStatisticsDtoFactory());
 
 		queue.startLogger();
-		
+
 		for (int i = 0; i < nbThreadProducer; i++) {
 			  new Thread(new ProducerThreadLog(queue), "ProducerThreadLog "+i).start();
 		 }
-		
+
 	}
 
-	
-	
-	
 
-	
+
+
+
+
 }
